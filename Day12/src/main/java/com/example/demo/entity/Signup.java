0729ddad8b65f4d769e8a74e46294c3c1e98bb92
Govendhiran7 <120083147+Govendhiran7@users.noreply.github.com>
2023-10-05 12:24:05 +0530

    Add files via upload

---
+++ Day12/src/main/java/com/example/demo/entity/Signup.java
@@ -0,0 +1,35 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="Signup")
+public class Signup {
+
+	@Id 
+	@Column(name="name")
+	private String name;
+	@Column(name="phno")
+	private String phno;
+	@Column(name="gender")
+	private String gender;
+	@Column(name="emailid")
+	private String emailid;
+	@Column(name="password")
+	private String password;
+	@Column(name="confirmpassword")
+	private String confirmpassword;
+	
+	
+}
